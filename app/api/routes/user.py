3931bbc912a2954771a31b1fe5faf4b5f32a7153
Alan Delgado <alanhd1302@gmail.com>
2024-12-04 04:39:23 -0500

    version estable de la base de datos

---
+++ app/api/routes/user.py
@@ -0,0 +1,24 @@
+import random
+from fastapi import APIRouter, FastAPI, HTTPException
+from fastapi.responses import FileResponse
+from pathlib import Path
+
+from app.config import BASE_USER_IMAGES_DIR, IP, MOUNT_USER_IMAGES_PATH
+from app.services import ZodiPairDB
+from app.models import GetRandomUsersModel, GetUserModel, GetProfileModel, UserListModel, UserValidationModel
+
+router = APIRouter(prefix="/users", tags=["Users"])
+
+db = ZodiPairDB()
+
+@router.post("/user-validation")
+async def post_user_validation(user_validation: UserValidationModel):
+    get_user: GetUserModel = db.get_user(user_validation)
+
+    return get_user
+
+@router.post("/get-profile")
+async def post_user_validation(user_id: str):
+    get_profile: GetProfileModel = db.get_profile(user_id)
+
+    return get_profile
--- app/main.py
+++ app/main.py
@@ -4,11 +4,12 @@ from fastapi.staticfiles import StaticFiles
 from pathlib import Path
 
 from app.config import BASE_USER_IMAGES_DIR, MOUNT_USER_IMAGES_PATH
-from app.api.routes import users_images
+from app.api.routes import users_images, user
 
 app = FastAPI()
 
 app.include_router(users_images.router)
+app.include_router(user.router)
 
 # Monta el directorio de imágenes como un recurso estático
 app.mount(MOUNT_USER_IMAGES_PATH, StaticFiles(directory=BASE_USER_IMAGES_DIR), name="users")
