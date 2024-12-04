7a93b7609889c6f42d07464dc0de4f1f284fd7fa
Alan Delgado <alanhd1302@gmail.com>
2024-12-04 05:10:48 -0500

    version estable de la base de datos

--- app/api/routes/user.py
+++ app/api/routes/user.py
@@ -13,12 +13,14 @@ db = ZodiPairDB()
 
 @router.post("/user-validation")
 async def post_user_validation(user_validation: UserValidationModel):
+    print("User validation: ", user_validation)
     get_user: GetUserModel = db.get_user(user_validation)
 
     return get_user
 
 @router.post("/get-profile")
 async def post_user_validation(user_id: str):
+    print("user_id: ", user_id)
     get_profile: GetProfileModel = db.get_profile(user_id)
 
     return get_profile
